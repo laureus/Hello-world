a164d92f9a7ca637ae937474dd643fa4f44f876d
Yuqing Luo <laureus31@126.com>
2011-10-29 12:01:44 +1100

    test
    test

--- src/com/me/helloworld/AndroidHelloWorldActivity.java
+++ src/com/me/helloworld/AndroidHelloWorldActivity.java
@@ -3,7 +3,7 @@ package com.me.helloworld;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
+//import android.content.SharedPreferences;
 //import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.graphics.Color;
 import android.os.AsyncTask;
